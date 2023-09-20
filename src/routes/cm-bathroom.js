35e584632ec492d007de5c6464c11065fdff9803
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-09-20 15:29:33 +0200

    SQL for time

--- src/routes/cm-bathroom.js
+++ src/routes/cm-bathroom.js
@@ -3,7 +3,7 @@ const pool = require("../../db");
 const router = express.Router();
 router.get('', async (req, res) => {
     try {
-        const allConsumeCurrent = await pool.query('SELECT * FROM tb_cm_bathroom');
+        const allConsumeCurrent = await pool.query('SELECT * FROM tb_cm_bathroom ORDER BY id_cm_bathroom ASC');
         res.json(allConsumeCurrent.rows);
     } catch (err) {
         console.error(err.message);
