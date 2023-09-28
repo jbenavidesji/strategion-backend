d82a977a1e958d5ddacaceb73581f15a76b566ac
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-09-28 11:43:05 +0200

    new services for energy db

---
+++ src/routes/energy-consume-city-co2.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const pool = require("../../db");
+const router = express.Router();
+router.get('', async (req, res) => {
+    try {
+        const allEnergyGermany = await pool.query('SELECT * FROM tb_energy_consume_city_co2');
+        res.json(allEnergyGermany.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+module.exports = router;
