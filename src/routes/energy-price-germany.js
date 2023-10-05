905e0ac2453709968f31cd8e8cba51748d17e1c4
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-10-05 15:15:10 +0200

    New services und routes

--- src/routes/energy-price-germany.js
+++ src/routes/energy-price-germany.js
@@ -3,7 +3,7 @@ const pool = require("../../db");
 const router = express.Router();
 router.get('', async (req, res) => {
     try {
-        const allEnergyGermany = await pool.query('SELECT * FROM tb_energy_price_germany');
+        const allEnergyGermany = await pool.query('SELECT * FROM tb_energy_price_germany ORDER BY id_energy_price_germany ASC ');
         res.json(allEnergyGermany.rows);
     } catch (err) {
         console.error(err.message);
--- src/routes/notifications.js
+++ src/routes/notifications.js
@@ -3,7 +3,7 @@ const pool = require("../../db");
 const router = express.Router();
 router.get('', async (req, res) => {
     try {
-        const allNotifications = await pool.query('SELECT * FROM tb_notifications');
+        const allNotifications = await pool.query('SELECT * FROM tb_notifications ORDER BY RANDOM() LIMIT 1');
         res.json(allNotifications.rows);
     } catch (err) {
         console.error(err.message);
